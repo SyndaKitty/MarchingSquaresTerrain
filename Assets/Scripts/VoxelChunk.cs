84cc56e3e32e62912f40ff536cae02075400280d
SpencasaurusRex <SpencasaurusRex@gmail.com>
2020-05-16 01:49:46 -0400

    I'm getting a headache

--- Assets/Scripts/VoxelChunk.cs
+++ Assets/Scripts/VoxelChunk.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class VoxelChunk : MonoBehaviour
@@ -10,7 +12,8 @@ public class VoxelChunk : MonoBehaviour
     // solid if it is > 0. Every 4 voxels in a square make up a voxel configuration
     // that is used to generate a mesh and collider.
     public float[,] Terrain = new float[Size,Size];
-    
+    public Vector2Int ChunkCoord;
+
     void OnDrawGizmos()
     {
         // Give a representation of the terrain
@@ -21,6 +24,18 @@ public class VoxelChunk : MonoBehaviour
                 Gizmos.color = Terrain[x, y] > 0 ? Color.white : Color.black;
                 Gizmos.DrawSphere(new Vector3(x, y, 0) + transform.position, 0.1f);
             }
+        }
+
+        Gizmos.color = Color.white;
+        for (int i = 0; i < pathPoints.Count - 1; i++)
+        {
+            Gizmos.DrawLine((Vector3)pathPoints[i] + transform.position, (Vector3)pathPoints[i + 1] + transform.position);
+        }
+
+        Gizmos.color = new Color(1, 0, 0, 0.2f);
+        foreach (var point in edgePoints.Keys)
+        {
+            Gizmos.DrawCube(transform.position + (Vector3Int)point, Vector3.one);
         }
     }
 
@@ -96,27 +111,100 @@ public class VoxelChunk : MonoBehaviour
     
     // Lookup to determine which edge connect to which given a voxel configuration
     // 0: down, 1: left, 2: right, 3: up
-    // edgeConnections[configuration, edgeInput] -> edgeOutput
-    static int[,] edgeConnections = new[,]
+    // edgeConnections[chunkEdge, configuration, edgeInput] -> edgeOutput
+    static int[,,] edgeConnections = new[,,]
     {
-        { -1,-1,-1,-1 },
-        {  1, 0,-1,-1 },
-        {  2,-1, 0,-1 },
-        { -1, 2, 1,-1 },
-        { -1,-1, 3, 2 },
-        {  2, 3, 0, 1 },
-        {  3,-1,-1, 0 },
-        { -1, 3,-1, 1 },
-        { -1, 3,-1, 1 },
-        {  3,-1,-1, 0 },
-        {  1, 0, 3, 2 },
-        { -1,-1, 3, 2 },
-        { -1, 2, 1,-1 },
-        {  2,-1, 0,-1 },
-        {  1, 0,-1,-1 },
-        { -1,-1,-1,-1 }
+        {
+            // Chunk Center 
+            { -1,-1,-1,-1 }, {  1, 0,-1,-1 }, {  2,-1, 0,-1 }, { -1, 2, 1,-1 },
+            { -1,-1, 3, 2 }, {  2, 3, 0, 1 }, {  3,-1,-1, 0 }, { -1, 3,-1, 1 },
+            { -1, 3,-1, 1 }, {  3,-1,-1, 0 }, {  1, 0, 3, 2 }, { -1,-1, 3, 2 },
+            { -1, 2, 1,-1 }, {  2,-1, 0,-1 }, {  1, 0,-1,-1 }, { -1,-1,-1,-1 }
+        },
+        {
+            // Chunk Bottom
+            { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, { -1,-1,-1,-1 },
+            { -1,-1, 3, 2 }, { -1,-1, 3, 2 }, { -1,-1, 3, 2 }, { -1,-1, 3, 2 },
+            { -1, 3,-1, 1 }, { -1, 3,-1, 1 }, { -1, 3,-1, 1 }, { -1, 3,-1, 1 },
+            { -1, 2, 1,-1 }, { -1, 2, 1,-1 }, { -1, 2, 1,-1 }, { -1, 2, 1,-1 }
+        },
+        {
+            // Chunk Left
+            { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, {  2,-1, 0,-1 }, {  2,-1, 0,-1 },
+            { -1,-1, 3, 2 }, { -1,-1, 3, 2 }, {  3,-1,-1, 0 }, {  3,-1,-1, 0 },
+            { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, {  2,-1, 0,-1 }, {  2,-1, 0,-1 },
+            { -1,-1, 3, 2 }, { -1,-1, 3, 2 }, {  3,-1,-1, 0 }, {  3,-1,-1, 0 }
+        },
+        {
+            // Chunk Right
+            { -1,-1,-1,-1 }, {  1, 0,-1,-1 }, { -1,-1,-1,-1 }, {  1, 0,-1,-1 },
+            { -1,-1,-1,-1 }, {  1, 0,-1,-1 }, { -1,-1,-1,-1 }, {  1, 0,-1,-1 },
+            { -1, 3,-1, 1 }, {  3,-1,-1, 0 }, { -1, 3,-1, 1 }, {  3,-1,-1, 0 },
+            { -1, 3,-1, 1 }, {  3,-1,-1, 0 }, { -1, 3,-1, 1 }, {  3,-1,-1, 0 }
+        },
+        {
+            // Chunk Up
+            { -1,-1,-1,-1 }, {  1, 0,-1,-1 }, {  2,-1, 0,-1 }, { -1, 2, 1,-1 },
+            { -1,-1,-1,-1 }, {  1, 0,-1,-1 }, {  2,-1, 0,-1 }, { -1, 2, 1,-1 },
+            { -1,-1,-1,-1 }, {  1, 0,-1,-1 }, {  2,-1, 0,-1 }, { -1, 2, 1,-1 },
+            { -1,-1,-1,-1 }, {  1, 0,-1,-1 }, {  2,-1, 0,-1 }, { -1, 2, 1,-1 }
+        },
+        {
+            // Chunk Bottom-left
+            { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, { -1,-1,-1,-1 },
+            { -1,-1, 3, 2 }, { -1,-1, 3, 2 }, { -1,-1, 3, 2 }, { -1,-1, 3, 2 },
+            { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, { -1,-1,-1,-1 },
+            { -1,-1, 3, 2 }, { -1,-1, 3, 2 }, { -1,-1, 3, 2 }, { -1,-1, 3, 2 }
+        },
+        {
+            // Chunk Bottom-right
+            { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, { -1,-1,-1,-1 },
+            { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, { -1,-1,-1,-1 },
+            {  3,-1,-1, 0 }, {  3,-1,-1, 0 }, {  3,-1,-1, 0 }, {  3,-1,-1, 0 },
+            {  3,-1,-1, 0 }, {  3,-1,-1, 0 }, {  3,-1,-1, 0 }, {  3,-1,-1, 0 }
+        },
+        {
+            // Chunk Top-left
+            { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, {  2,-1, 0,-1 }, {  2,-1, 0,-1 },
+            { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, {  2,-1, 0,-1 }, {  2,-1, 0,-1 },
+            { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, {  2,-1, 0,-1 }, {  2,-1, 0,-1 },
+            { -1,-1,-1,-1 }, { -1,-1,-1,-1 }, {  2,-1, 0,-1 }, {  2,-1, 0,-1 }
+        },
+        {
+            // Chunk Top-right
+            { -1,-1,-1,-1 }, {  1, 0,-1,-1 }, { -1,-1,-1,-1 }, {  1, 0,-1,-1 },
+            { -1,-1,-1,-1 }, {  1, 0,-1,-1 }, { -1,-1,-1,-1 }, {  1, 0,-1,-1 },
+            { -1,-1,-1,-1 }, {  1, 0,-1,-1 }, { -1,-1,-1,-1 }, {  1, 0,-1,-1 },
+            { -1,-1,-1,-1 }, {  1, 0,-1,-1 }, { -1,-1,-1,-1 }, {  1, 0,-1,-1 }
+        },
     };
 
+    //  7   4   8
+    //   |-----|
+    // 2 |  0  | 3 
+    //   |-----|
+    //  5   1   6
+    static int[] ChunkEdgeLookup = new[] { 5, 1, 6, 2, 0, 3, 7, 4, 8};
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    int ChunkEdgeIndex(int x, int y) => ChunkEdgeLookup[
+        Mathf.CeilToInt((float)x / (Size - 1)) + 
+        Mathf.CeilToInt((float)y / (Size - 1)) * 3];
+
+    // Probably can avoid this lookup table by just adding all chunk edges, then checking for -1 when following edges
+    // int[,] ValidEdgeLookup = new[]
+    // {
+    //     { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 }, // center
+    //     { }, // bottom
+    //     { }, // left
+    //     { }, // right
+    //     { }, // up
+    //     { }, // bottom-left
+    //     { }, // bottom-right
+    //     { }, // top-left
+    //     { }  // top-right
+    // };
+    
     // Lookup to determine where an edge leads
     // edgeOffset[edge] -> voxel offset
     static Vector2Int[] edgeOffset = new[]
@@ -127,7 +215,9 @@ public class VoxelChunk : MonoBehaviour
         new Vector2Int(0, 1),
     };
     
-    public void GenerateMesh()
+    List<Vector2> pathPoints = new List<Vector2>();
+    Dictionary<Vector2Int, int> edgePoints = new Dictionary<Vector2Int, int>();
+    public IEnumerator GenerateMesh(VoxelTerrain voxelTerrain)
     {
         MeshFilter meshFilter = GetComponent<MeshFilter>();
         PolygonCollider2D polygonCollider = GetComponent<PolygonCollider2D>();
@@ -140,29 +230,61 @@ public class VoxelChunk : MonoBehaviour
         int indexOffset = 0;
         
         // Track which voxel configurations have edges
-        Dictionary<Vector2Int, int> edgePoints = new Dictionary<Vector2Int, int>();
+        //Dictionary<Vector2Int, int> edgePoints = new Dictionary<Vector2Int, int>();
         // Remember the configuration(lookup) for each voxel square
         int[,] lookupGrid = new int[Size,Size];
+        float[,] expandedTerrain = new float[Size, Size];
+        int[,] chunkEdgeLookup = new int[Size, Size];
 
-        for (int y = 0; y < Size - 1; y++)
+        for (int y = 0; y < Size; y++)
         {
-            for (int x = 0; x < Size - 1; x++)
+            for (int x = 0; x < Size; x++)
             {
-                offsetPoint.x = x;
-                offsetPoint.y = y;
-                
                 int lookup = 0;
-                // Calculate lookup
-                for (int offset = 0; offset < 4; offset++)
+
+                if (x < Size - 1 && y < Size - 1)
                 {
-                    var o = offsets[offset];
-                    lookup += Terrain[x + o.x, y + o.y] > 0f ? (1 << offset) : 0;
+                    for (int offset = 0; offset < 4; offset++)
+                    {
+                        var o = offsets[offset];
+                        var v = Terrain[x + o.x, y + o.y];
+                        expandedTerrain[x, y] = v;
+                        lookup += v > 0f ? (1 << offset) : 0;
+                    }
+                }
+                else
+                {
+                    for (int offset = 0; offset < 4; offset++)
+                    {
+                        var o = offsets[offset];
+                        var v = voxelTerrain.GetVoxel(
+                            ChunkCoord.x * Size + x + o.x, 
+                            ChunkCoord.y * Size + y + o.y
+                        );
+                        expandedTerrain[x, y] = v;
+                        lookup += v > 0f ? (1 << offset) : 0;
+                    }
                 }
 
                 lookupGrid[x, y] = lookup;
-                if (lookup > 0 && lookup < 15)
+                chunkEdgeLookup[x, y] = ChunkEdgeIndex(x, y);
+            }
+        }
+
+
+        for (int y = 0; y < Size; y++)
+        {
+            for (int x = 0; x < Size; x++)
+            {
+                offsetPoint.x = x;
+                offsetPoint.y = y;
+
+                int lookup = lookupGrid[x, y];
+                int chunkEdge = chunkEdgeLookup[x, y];
+
+                if (x < Size - 1 && y < Size - 1 && (lookup > 0 && lookup < 15 || chunkEdge != 0))
                 {
-                    edgePoints.Add(new Vector2Int(x, y), lookup == 5 || lookup == 10 ? 2 : 1);
+                    edgePoints.Add(new Vector2Int(x, y), chunkEdge == 0 && (lookup == 5 || lookup == 10) ? 2 : 1);
                 }
 
                 // Add points
@@ -198,13 +320,13 @@ public class VoxelChunk : MonoBehaviour
         
         while (edgePoints.Any())
         {
-            List<Vector2> pathPoints = new List<Vector2>();
+            // List<Vector2> pathPoints = new List<Vector2>();
             
             // Starting at the first point in the queue
             var point = edgePoints.Keys.First();
             var startingPoint = point;
 
-            int lookup = lookupGrid[point.x, point.y];
+            int lookup = lookupGrid[point.x,point.y];
             int nextConnection = -1;
 
             edgePoints[point] = edgePoints[point] - 1;
@@ -213,7 +335,8 @@ public class VoxelChunk : MonoBehaviour
             // Find the first valid edge for the point
             for (int i = 0; i < 4; i++)
             {
-                int connection = edgeConnections[lookup, i];
+                int chunkEdge = ChunkEdgeIndex(point.x, point.y);
+                int connection = edgeConnections[chunkEdge, lookup, i];
                 // Valid connection that leads to another edge
                 if (connection >= 0 && edgePoints.ContainsKey(point + edgeOffset[connection]))
                 {
@@ -223,6 +346,9 @@ public class VoxelChunk : MonoBehaviour
                 }
             }
 
+            // If no valid connection, ignore this point
+            if (nextConnection == -1) continue;
+            
             point += edgeOffset[nextConnection];
             
             // Keep following the edges that are connected to our starting point
@@ -238,9 +364,11 @@ public class VoxelChunk : MonoBehaviour
                 
                 // Add 4 to get the cardinal points instead of diagonals
                 pathPoints.Add(basePoints[incomingEdge + 4] + (Vector3Int)point);
+                yield return new WaitForSeconds(0.5f);
                 
-                lookup = lookupGrid[point.x, point.y];
-                nextConnection = edgeConnections[lookup, incomingEdge];
+                int chunkEdge = ChunkEdgeIndex(point.x, point.y);
+                lookup = lookupGrid[point.x,point.y];
+                nextConnection = edgeConnections[chunkEdge, lookup, incomingEdge];
                 var nextPoint = point + edgeOffset[nextConnection];
 
                 point = nextPoint;
--- Assets/Scripts/VoxelTerrain.cs
+++ Assets/Scripts/VoxelTerrain.cs
@@ -39,16 +39,16 @@ public class VoxelTerrain : MonoBehaviour
             alteredChunks.Add(chunkCoord);
 
             // Temp code.. REMOVE
-            // if (blockCoord.x == 0) 
-            //     c.Terrain[blockCoord.x, blockCoord.y] = -1;
-            if (blockCoord.y == 0)
-                c.Terrain[blockCoord.x, blockCoord.y] = -1;
-            if (blockCoord.x == VoxelChunk.Size - 1)
-                c.Terrain[blockCoord.x, blockCoord.y] = -1;
-            if (blockCoord.y == VoxelChunk.Size - 1)
-                c.Terrain[blockCoord.x, blockCoord.y] = -1;
-            
-            
+            //if (blockCoord.x == 0)
+            //    c.Terrain[blockCoord.x, blockCoord.y] = -1;
+            //if (blockCoord.y == 0)
+            //    c.Terrain[blockCoord.x, blockCoord.y] = -1;
+            //if (blockCoord.x == VoxelChunk.Size - 1)
+            //    c.Terrain[blockCoord.x, blockCoord.y] = -1;
+            //if (blockCoord.y == VoxelChunk.Size - 1)
+            //    c.Terrain[blockCoord.x, blockCoord.y] = -1;
+
+
             if (blockCoord.x == 0)
                 alteredChunks.Add(chunkCoord + Vector2Int.left);
             if (blockCoord.x == VoxelChunk.Size - 1)
@@ -62,8 +62,24 @@ public class VoxelTerrain : MonoBehaviour
         foreach (var alteredChunk in alteredChunks)
         {
             var c = GetChunk(alteredChunk);
-            c.GenerateMesh();
+            StartCoroutine(c.GenerateMesh(this));
         }
+    }
+
+    public float GetVoxel(int voxelX, int voxelY)
+    {
+        var t = GetChunk(voxelX, voxelY, out var cc).Terrain;
+        return t[voxelX - cc.x * VoxelChunk.Size, voxelY - cc.y * VoxelChunk.Size];
+    }
+
+    VoxelChunk GetChunk(int voxelX, int voxelY, out Vector2Int cc)
+    {
+        cc = new Vector2Int(
+            Mathf.FloorToInt((float) voxelX / VoxelChunk.Size),
+            Mathf.FloorToInt((float) voxelY / VoxelChunk.Size)
+        );
+
+        return GetChunk(cc);
     }
 
     VoxelChunk GetChunk(Vector2Int chunkCoord)
@@ -83,6 +99,7 @@ public class VoxelTerrain : MonoBehaviour
         chunk.transform.localPosition = (Vector3Int)chunkCoords * VoxelChunk.Size;
 
         var vc = chunk.GetComponent<VoxelChunk>();
+        vc.ChunkCoord = chunkCoords;
         chunks[chunkCoords] = vc;
 
         return vc;
